734e9dcb01aa7583e8ecaa0173f8eea17ca9f774
RinatTr <rinatreg@gmail.com>
2019-08-22 16:19:16 -0400

    old version name

---
+++ v1/precache-manifest.893fb6f0271503786fdd31c093b69219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7f70897d0ddf96ffd1e3d1d1f46ad0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e838733f2b420977a168",
+    "url": "/static/css/main.e0950480.chunk.css"
+  },
+  {
+    "revision": "a6ceaeb1e75c41d42b96",
+    "url": "/static/js/2.17c0becd.chunk.js"
+  },
+  {
+    "revision": "e838733f2b420977a168",
+    "url": "/static/js/main.4141de55.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "732829016a428c75c316d1069520c5d8",
+    "url": "/static/media/resume.73282901.pdf"
+  },
+  {
+    "revision": "1f97bc116ae17ed68c7f0d92127a0716",
+    "url": "/static/media/rinat.1f97bc11.png"
+  }
+]);
