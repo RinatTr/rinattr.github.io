47173d439bfffbfffdd5784135dccc889ec4f0a7
RinatTr <43793455+RinatTr@users.noreply.github.com>
2023-12-16 17:39:42 -0500

    Add files via upload

---
+++ precache-manifest.b95e9018c38524092f5ec4e50f7da5fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282dc66a65093b14cce8517c030000d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "359d09bff5b3d688ca19",
+    "url": "/static/css/main.36eb8328.chunk.css"
+  },
+  {
+    "revision": "d5124068b744d76eb90d",
+    "url": "/static/js/2.9a154e1f.chunk.js"
+  },
+  {
+    "revision": "359d09bff5b3d688ca19",
+    "url": "/static/js/main.44db2331.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "93c397a23ecbcd524db11618d288b319",
+    "url": "/static/media/pi-thumb.93c397a2.png"
+  },
+  {
+    "revision": "02fca2d62e2b9a0395d2e699220a4871",
+    "url": "/static/media/resume.02fca2d6.pdf"
+  },
+  {
+    "revision": "a2e2649a330346f5d45cc342e825d66e",
+    "url": "/static/media/rinat.a2e2649a.jpg"
+  }
+]);
