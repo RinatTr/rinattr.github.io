050a3ae73f2df23217da040c7c1becc62138dbe8
RinatTr <43793455+RinatTr@users.noreply.github.com>
2023-12-20 16:24:54 -0500

    Add files via upload

---
+++ precache-manifest.5d23c714c23352eea9d3f3ac5d399ca1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4efebbd7ed630f366bf36d76dd19e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "730bf69bbc4d0abac8b0",
+    "url": "/static/css/main.e98dc9e1.chunk.css"
+  },
+  {
+    "revision": "d5124068b744d76eb90d",
+    "url": "/static/js/2.9a154e1f.chunk.js"
+  },
+  {
+    "revision": "730bf69bbc4d0abac8b0",
+    "url": "/static/js/main.d8d8e1e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "93c397a23ecbcd524db11618d288b319",
+    "url": "/static/media/pi-thumb.93c397a2.png"
+  },
+  {
+    "revision": "fb4646abe9405997b3227bf2e1227285",
+    "url": "/static/media/post_modal.fb4646ab.png"
+  },
+  {
+    "revision": "02fca2d62e2b9a0395d2e699220a4871",
+    "url": "/static/media/resume.02fca2d6.pdf"
+  },
+  {
+    "revision": "ac1ed860a169082302914daf4bdce8a4",
+    "url": "/static/media/rinat.ac1ed860.jpg"
+  },
+  {
+    "revision": "01a5560514746feadded87e5d7962991",
+    "url": "/static/media/ringedin.01a55605.png"
+  },
+  {
+    "revision": "01a5560514746feadded87e5d7962991",
+    "url": "/static/media/win-game.01a55605.png"
+  }
+]);
